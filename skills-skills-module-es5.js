7bba50cfb1a329ba1983bd35937a933a1560fee3
krishna kanajam <57392565+krishnakanajam@users.noreply.github.com>
2020-12-24 15:59:49 +0530

    Add files via upload

---
+++ skills-skills-module-es5.js
@@ -0,0 +1,219 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["skills-skills-module"], {
+  /***/
+  "./node_modules/raw-loader/dist/cjs.js!./src/app/skills/skills.component.html":
+  /*!************************************************************************************!*\
+    !*** ./node_modules/raw-loader/dist/cjs.js!./src/app/skills/skills.component.html ***!
+    \************************************************************************************/
+
+  /*! exports provided: default */
+
+  /***/
+  function node_modulesRawLoaderDistCjsJsSrcAppSkillsSkillsComponentHtml(module, __webpack_exports__, __webpack_require__) {
+    "use strict";
+
+    __webpack_require__.r(__webpack_exports__);
+    /* harmony default export */
+
+
+    __webpack_exports__["default"] = "<div class=\"col-sm-10 col-md-8 col-sm-offset-1 col-md-offset-2 p0\">\n\n  <h3>Skills</h3>\n\n</div>\n\n<div class=\"col-sm-10 col-md-8 col-sm-offset-1 col-md-offset-2 panel\">\n\n\n  <div class=\"col-sm-12 pad\">\n\n    <h4 class=\"back\">Programming Languages & Technologies</h4>\n\n  </div>\n\n  <div class=\"col-sm-12 pad\">\n\n    <figure class=\"snip1566\">\n      <img src=\"./assets/images/icons/html5.jpg\" class=\"img-responsive\" />\n    </figure>\n    <figure class=\"snip1566\">\n      <img src=\"./assets/images/icons/css3.png\" class=\"img-responsive\" />\n\n    </figure>\n    <figure class=\"snip1566\">\n      <img src=\"./assets/images/icons/bootstrap.png\" class=\"img-responsive\" />\n\n    </figure>\n    <figure class=\"snip1566\">\n      <img src=\"./assets/images/icons/javas.png\" class=\"img-responsive\" />\n\n    </figure>\n    <figure class=\"snip1566\">\n      <img src=\"./assets/images/icons/jquery.png\" class=\"img-responsive\" />\n\n    </figure>\n\n    <figure class=\"snip1566\">\n      <img src=\"./assets/images/icons/sass.png\" class=\"img-responsive\" />\n\n    </figure>\n\n    <figure class=\"snip1566\">\n\n      <img src=\"./assets/images/icons/angular.png\" class=\"img-responsive\" />\n    </figure>\n\n    <figure class=\"snip1566\">\n      <img src=\"./assets/images/icons/photoshop.png\" class=\"img-responsive\" />\n    </figure>\n\n    <figure class=\"snip1566\">\n      <img src=\"./assets/images/icons/illustrator.jpg\" class=\"img-responsive\" />\n    </figure>\n\n    <figure class=\"snip1566\">\n      <img src=\"./assets/images/icons/coraldraw.png\" class=\"img-responsive\" />\n    </figure>\n\n    <br />\n    <br />\n\n\n    <p class=\"para\" style=\"text-indent: 0%;\">\n\n      <i class=\"far fa-hand-point-right\"></i> Trained in all above Languages & Techonoligies And also\n      Certified too.\n\n      <a data-magnify=\"gallery\" data-caption=\"Training Certificate\" data-group=\"\" class=\"pop\" data-toggle=\"tooltip\"\n        data-placement=\"right\" title=\"click here to see Certificate\" href=\"./assets/images/goodrichit.jpg\"><i\n          class=\"fas fa-award\"></i></a>\n\n    </p>\n\n    <a href=\"#\" data-toggle=\"tooltip\" data-placement=\"right\" title=\"Hooray!\">Right</a>\n\n\n  </div>\n\n\n  <div class=\"col-lg-8 col-md-10 col-sm-12 pad\">\n\n    <h4 class=\"back\">Technical Skills</h4>\n\n\n    <table class=\"about\" border=\"1\">\n      <tr>\n        <td><i class=\"far fa-hand-point-right\"></i>&nbsp;&nbsp;&nbsp;Responsive web designs in\n          multiple screen sizes/devices.</td>\n\n      </tr>\n\n      <tr>\n        <td><i class=\"far fa-hand-point-right\"></i>&nbsp;&nbsp;&nbsp;Expert in Identifying and\n          Fixing UI issues.</td>\n\n      </tr>\n\n      <tr>\n        <td><i class=\"far fa-hand-point-right\"></i>&nbsp;&nbsp;&nbsp;Having Knowledge and Idea\n          about the Backend & Databases.</td>\n\n      </tr>\n      <tr>\n        <td><i class=\"far fa-hand-point-right\"></i>&nbsp;&nbsp;&nbsp;Knowing the Multi models and\n          snippets for the designing applications. </td>\n\n      </tr>\n      <tr>\n        <td><i class=\"far fa-hand-point-right\"></i>&nbsp;&nbsp;&nbsp;Ability to design the pages in\n          short period of time as per client requirement.</td>\n\n      </tr>\n\n\n    </table>\n\n\n  </div>\n\n</div>\n";
+    /***/
+  },
+
+  /***/
+  "./src/app/routing/skills.ts":
+  /*!***********************************!*\
+    !*** ./src/app/routing/skills.ts ***!
+    \***********************************/
+
+  /*! exports provided: skillsroutes */
+
+  /***/
+  function srcAppRoutingSkillsTs(module, __webpack_exports__, __webpack_require__) {
+    "use strict";
+
+    __webpack_require__.r(__webpack_exports__);
+    /* harmony export (binding) */
+
+
+    __webpack_require__.d(__webpack_exports__, "skillsroutes", function () {
+      return skillsroutes;
+    });
+    /* harmony import */
+
+
+    var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(
+    /*! tslib */
+    "./node_modules/tslib/tslib.es6.js");
+    /* harmony import */
+
+
+    var _skills_skills_component__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(
+    /*! ../skills/skills.component */
+    "./src/app/skills/skills.component.ts");
+
+    const skillsroutes = [{
+      path: '',
+      component: _skills_skills_component__WEBPACK_IMPORTED_MODULE_1__["SkillsComponent"]
+    }];
+    /***/
+  },
+
+  /***/
+  "./src/app/skills/skills.component.css":
+  /*!*********************************************!*\
+    !*** ./src/app/skills/skills.component.css ***!
+    \*********************************************/
+
+  /*! exports provided: default */
+
+  /***/
+  function srcAppSkillsSkillsComponentCss(module, __webpack_exports__, __webpack_require__) {
+    "use strict";
+
+    __webpack_require__.r(__webpack_exports__);
+    /* harmony default export */
+
+
+    __webpack_exports__["default"] = "\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzcmMvYXBwL3NraWxscy9za2lsbHMuY29tcG9uZW50LmNzcyJ9 */";
+    /***/
+  },
+
+  /***/
+  "./src/app/skills/skills.component.ts":
+  /*!********************************************!*\
+    !*** ./src/app/skills/skills.component.ts ***!
+    \********************************************/
+
+  /*! exports provided: SkillsComponent */
+
+  /***/
+  function srcAppSkillsSkillsComponentTs(module, __webpack_exports__, __webpack_require__) {
+    "use strict";
+
+    __webpack_require__.r(__webpack_exports__);
+    /* harmony export (binding) */
+
+
+    __webpack_require__.d(__webpack_exports__, "SkillsComponent", function () {
+      return SkillsComponent;
+    });
+    /* harmony import */
+
+
+    var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(
+    /*! tslib */
+    "./node_modules/tslib/tslib.es6.js");
+    /* harmony import */
+
+
+    var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(
+    /*! @angular/core */
+    "./node_modules/@angular/core/fesm2015/core.js");
+
+    let SkillsComponent = class SkillsComponent {
+      constructor() {}
+
+      ngOnInit() {
+        $(document).ready(function () {
+          // for tooltips
+          $('[data-toggle="tooltip"]').tooltip(); // modal popups
+
+          $('[data-magnify=gallery]').magnify({
+            // Enable modal to drag
+            draggable: true,
+            // Enable modal to resize
+            resizable: true,
+            // Enable image to move
+            movable: true,
+            // Enable keyboard navigation
+            keyboard: true,
+            // Shows the title
+            title: true,
+            // Min width of modal
+            modalWidth: 350,
+            // Min height of modal
+            modalHeight: 350,
+            // Disable the image viewer maximized on init
+            initMaximized: true
+          });
+        });
+      }
+
+    };
+    SkillsComponent = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Component"])({
+      selector: 'app-skills',
+      template: tslib__WEBPACK_IMPORTED_MODULE_0__["__importDefault"](__webpack_require__(
+      /*! raw-loader!./skills.component.html */
+      "./node_modules/raw-loader/dist/cjs.js!./src/app/skills/skills.component.html")).default,
+      styles: [tslib__WEBPACK_IMPORTED_MODULE_0__["__importDefault"](__webpack_require__(
+      /*! ./skills.component.css */
+      "./src/app/skills/skills.component.css")).default]
+    }), tslib__WEBPACK_IMPORTED_MODULE_0__["__metadata"]("design:paramtypes", [])], SkillsComponent);
+    /***/
+  },
+
+  /***/
+  "./src/app/skills/skills.module.ts":
+  /*!*****************************************!*\
+    !*** ./src/app/skills/skills.module.ts ***!
+    \*****************************************/
+
+  /*! exports provided: SkillsModule */
+
+  /***/
+  function srcAppSkillsSkillsModuleTs(module, __webpack_exports__, __webpack_require__) {
+    "use strict";
+
+    __webpack_require__.r(__webpack_exports__);
+    /* harmony export (binding) */
+
+
+    __webpack_require__.d(__webpack_exports__, "SkillsModule", function () {
+      return SkillsModule;
+    });
+    /* harmony import */
+
+
+    var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(
+    /*! tslib */
+    "./node_modules/tslib/tslib.es6.js");
+    /* harmony import */
+
+
+    var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(
+    /*! @angular/core */
+    "./node_modules/@angular/core/fesm2015/core.js");
+    /* harmony import */
+
+
+    var _angular_common__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(
+    /*! @angular/common */
+    "./node_modules/@angular/common/fesm2015/common.js");
+    /* harmony import */
+
+
+    var _angular_router__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(
+    /*! @angular/router */
+    "./node_modules/@angular/router/fesm2015/router.js");
+    /* harmony import */
+
+
+    var _skills_skills_component__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(
+    /*! ../skills/skills.component */
+    "./src/app/skills/skills.component.ts");
+    /* harmony import */
+
+
+    var _routing_skills__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(
+    /*! ../routing/skills */
+    "./src/app/routing/skills.ts");
+
+    let SkillsModule = class SkillsModule {};
+    SkillsModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+      declarations: [_skills_skills_component__WEBPACK_IMPORTED_MODULE_4__["SkillsComponent"]],
+      imports: [_angular_common__WEBPACK_IMPORTED_MODULE_2__["CommonModule"], _angular_router__WEBPACK_IMPORTED_MODULE_3__["RouterModule"].forChild(_routing_skills__WEBPACK_IMPORTED_MODULE_5__["skillsroutes"])]
+    })], SkillsModule);
+    /***/
+  }
+}]);
+//# sourceMappingURL=skills-skills-module-es5.js.map
